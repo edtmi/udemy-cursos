a0b7d5788cff210b8a8b1b98893aa5467dd9b8b6
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-10-17 10:39:56 -0300

    :build: rendering Header component in _app.tsx

--- board/src/pages/_app.tsx
+++ board/src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from "next/app";
+import React from "react";
+import { Header } from "../components/Header";
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Header />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
