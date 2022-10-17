e48133cb1e86ebe2c8739a2ef02048356f6ae45e
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-10-17 14:28:26 -0300

    :build: adding and styling main + section tags in home component

--- board/src/pages/index.tsx
+++ board/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import styles from "../styles/home.module.scss";
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -7,9 +7,25 @@ export default function Home() {
       <Head>
         <title>Board - Organizando suas tarefas</title>
       </Head>
-      <h1 className={styles.title}>
-        Primeiro projeto com <span>NextJS</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Ferramenta board" />
+
+        <section className={styles.callToAction}>
+          <h1>
+            Uma ferramenta para seu dia a dia Escreva, planeje e organize-se...
+          </h1>
+          <p>
+            <span>100% Gratuita</span> e online
+          </p>
+        </section>
+
+        <div className={styles.donaters}>
+          <h3>Apoiadores:</h3>
+          <div className={styles.donaterImg}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/35/Gira_internacional_USA_-_Steve_Jobs_%28cropped%29.jpg" alt="Usuário 1" />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
